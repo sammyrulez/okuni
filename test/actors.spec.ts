94ad252b0d0c4b4cf02d6ddee66d078ddef71b4b
Samuele Reghenzi <sam@MacBook-Pro-di-Samuele.local>
2018-09-16 07:45:22 +0200

    proper testing

--- test/actors.spec.ts
+++ test/actors.spec.ts
@@ -1,18 +1,37 @@
-import {describe} from 'mocha';
-import {Scene} from '../lib/scene';
+import { describe , it} from 'mocha';
+import { Scene } from '../lib/scene';
 import { ActorRef, Actor } from '../lib/actor';
 
 class TestActor implements Actor<string> {
     tell: (message: string) => Promise<void> = async (m: String) => {
-          console.log('message ' + m);
-           
+        console.log('message ' + m);
+
     }
 
 }
 
-describe('A scene could create and Actor', function() {
+class StateActor implements Actor<string> {
+    private readonly guests: string[] = [];
+    tell: (message: string) => Promise<void> = async (m: string) => {
+        this.guests.push(m);
+        console.log('guests ' + this.guests);
 
-    const ref: ActorRef<String> = new Scene().pullActor(TestActor, null);
-    ref.tell('Ciao');
+    }
+
+}
+
+const scene = new Scene();
+
+describe('An Actor', function () {
+    it('should be initialized by a scene', async function () {
+       
+        const testRef: ActorRef<string> = scene.pullActor(TestActor, null);
+        testRef.tell('Ciao');
     });
-  
+    it('should keep its state', async function() {
+        const stateRef: ActorRef<string> = scene.pullActor(StateActor, null);
+        stateRef.tell('Bob');
+        stateRef.tell('Alice');
+        stateRef.tell('Mike');
+    });
+});
